4625e2bdd98cba70cef4a2b4d5bd28d97af7df94
ejhb <bonurajosh13@gmail.com>
2020-02-10 16:32:48 +0100

    add sql script

---
+++ 2020-02-10_application_sebastien.sql
@@ -0,0 +1,79 @@
+USE sakila;
+
+WITH act_count AS 
+(
+	SELECT
+	a.first_name
+	,a.last_name
+    ,COUNT(*) nb_film
+	FROM actor a 
+	INNER JOIN film_actor  ON fa.actor_id = a.action_id
+	GROUP BY 
+	a.first_name
+	,a.last_name
+)
+SELECT * 
+FROM act_count a
+WHERE NOT EXISTS
+;
+
+
+-- film jamais joué 
+SELECT 
+f.title
+,f.release_year
+FROM film f
+LEFT JOIN inventory i ON f.film_id = i.film_id
+LEFT JOIN rental r ON r.inventory_id = i.inventory_id
+WHERE r.rental_id IS NULL;
+
+
+-- Film jamais loué avec "NOT IN"
+SELECT 
+f.title
+,f.release_year
+FROM film f
+WHERE f.film_id NOT IN 
+(
+	SELECT f2.film_id
+	FROM film f2
+	INNER JOIN inventory i on f2.film_id = i.film_id
+	INNER JOIN rental r on r.inventory_id = i.inventory_id
+);
+
+-- Temps moyen de restitution d'un film 
+
+SELECT AVG(ABS(DATEDIFF(return_date,rental_date)))
+FROM rental;
+
+-- Temps moyen de restitution d'un film par client (classement du plus long au plus court)
+
+
+SELECT 
+c.first_name
+,c.last_name
+,AVG(ABS(DATEDIFF(return_date,rental_date))) avg_duration
+FROM rental r 
+INNER JOIN customer c
+ON c.customer_id = r.customer_id
+GROUP BY 
+c.first_name
+,c.last_name
+ORDER BY 3 DESC
+;
+
+-- Temps moyen de restitution d'un film par client (classement du plus long au plus court) Si au dessus de la moyenne = mauvais si en dessous = bon 
+
+SELECT r.first_name
+,r.last_name
+,AVG(ABS(DATEDIFF( return_date , rental_date))),
+CASE 
+    WHEN AVG(ABS(DATEDIFF( return_date , rental_date))) <= 5 THEN 'Good customer'
+    ELSE 'Bad customer'
+END AS quality
+FROM rental r 
+INNER JOIN customer c 
+ON r.customer_id = c.customer_id
+GROUP BY first_name, last_name;
+
+
