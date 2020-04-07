8762bd4380ba0f5e12720a7131a131f112860f1f
ejhb <bonurajosh13@gmail.com>
2020-04-07 14:36:54 +0200

    update

--- 2020-04-01-creature/test.py
+++ 2020-04-01-creature/test.py
@@ -1,3 +1,15 @@
+# Principe du jeu :
+# Des créature se déplace sur un damier, d’une case à la fois (Les diagonales sont autorisées). Si l’une d’elles arrive sur la case de l’autre, elle la capture et à gagné.
+# Pour cela on va définir 3 classes :
+# • Case, qui possède un attribut x et un attribut y.
+# • Créature, qui possède un attribut nom, position.
+# • Jeu, qui possède quatre attributs listesDesCases, listeDesCreatures, tour, et actif. 
+
+# 1. Rédiger ces classe avec leurs constructeurs. Modifier leur méthode __str__ de façon à rendre leur affichage utile.
+# 2. Rédiger une methode estOccupee(case) dans la classe Jeu qui vérifie si la Case case est occupée.
+# 3. Rédiger une méthode adjacentes(jeu) dans la classe Case, qui renvoie la liste des Case du jeu adjacentes à la case courante.
+
+# Classe d'un case et des case adjacente 
 class case:
     def __init__(self, x, y):
         self.x = x
@@ -11,6 +23,7 @@ class case:
     def __str__(self):
         return 'La case positionné en ' + str(self.x) + ' de la largeur et ' + str(self.y) + ' de la longeur.'
 
+# Fonction qui génère un plateau par les inputs de l'utilisateur.
 def generate():
     xy = int(input("Veuillez entrer la largeur :\n"))
     yx = int(input("Veuillez entrer la longueur :\n"))
@@ -23,7 +36,43 @@ def generate():
         print(i)
     return plateau
 
+# 4. Rédiger une méthode choisirCible(jeu) dans la class Creature qui renvoie la case voisine occupée s’il en existe une, et une voisine aléatoire sinon 
+# 5. Rédiger une méthode deplacer(créature, case) dans la class Jeu qui, si c’est autorisé, déplace créature sur case, affiche le nom du vainqueur dans la console si la case case était occupée par une autre Creature, et sinon incrémente le tour et échange la Creature active (c’est désormais le tour de l’autre de se déplacer).
+# 6. Initialisé un plateau de taille 4x4 avec deux Creature positionnées dans les angles oposés.
+# 7. Créez un programme où deux Creature s’affrontent en se déplaçant à l’aide de la méthode choisirCible(jeu) jusqu’à ce que l’une ait gagné.
 
+
+class creature:
+    def __init__(self, nom, position):
+        self.nom = nom
+        self.position = position
+        listeCreatures.append(nom)
+    def choisirCible(jeu):
+        for i in case.caseAdjacentes:
+            if jeu.estOccupee(i):
+                return case
+            else:
+                return randrange(case.caseAdjacentes)
+    def __str__(self):
+        return 'la creature ' + self.nom + ' à été créee et positionné à ' + str(self.position)
+    
+    
+class jeu:
+    def __init__(self, listeCases, listeCreature, tour, actif):
+        self.listeCases = listeCases
+        self.listeCreature = listeCreature
+        self.tour = tour
+        self.actif = actif
+        tour = 0
+    def estOccupee(case):
+            if case != " ":
+                return case
+
+    def deplacer(creature, case):
+        creature.choisirCible()
+        if case == creature.position:
+            print(creature.nom + "à gagner")
+        else: jeu.tour += 1
 
 
 
