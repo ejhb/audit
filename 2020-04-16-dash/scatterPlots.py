3418449b562043e8cec7acaf60e00670f2bf5aa5
ejhb <bonurajosh13@gmail.com>
2020-04-16 11:54:47 +0200

    update

--- 2020-04-16-dash/scatterPlots.py
+++ 2020-04-16-dash/scatterPlots.py
@@ -4,13 +4,12 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 
+Debug = True
+
 app = dash.Dash()
 
 df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
+    'https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
 
 app.layout = html.Div([
@@ -35,12 +34,12 @@ app.layout = html.Div([
                 xaxis={'type': 'log', 'title': 'GDP Per Capita'},
                 yaxis={'title': 'Life Expectancy'},
                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
+                legend={'x': 0, 'y': 1.20},
+                hovermode= 'closest'
             )
         }
     )
 ])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
