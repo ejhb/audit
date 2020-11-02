9f3205e056b41989c9216a4ed61352924b021b24
ejhb <bonurajosh13@gmail.com>
2020-11-03 00:57:09 +0100

    update

--- 2020-11-02-brief-car/import_csv_db.py
+++ 2020-11-02-brief-car/import_csv_db.py
@@ -1,5 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+
+# from sqlalchemy import create_engine
+# import pandas as pd
+# import time
+
+
+# engine = create_engine("mysql+pymysql://root@localhost/dataAi")
+
+# column = "Car_Name  Year    Selling_Price   Present_Price   Kms_Driven    Fuel_Type     Seller_Type     Transmission    Owner"
+
+# def chargement(x, link, table):
+    
+#     print("Lecture des données")
+#     col = x.split()
+#     start_time = time.time()
+#     df = pd.read_csv(link, encoding="UTF-8")
+#     df.columns = col
+#     print("Données lu")
+#     df.to_sql(table, con = engine, if_exists='append', index=False)
+#     return print("Temps d execution : %s secondes ---" % (time.time() - start_time))
+
+
+# chargement(column,'2020-11-02-brief-car/carData.csv', 'cardata')
+
 
 from sqlalchemy import create_engine
 import pandas as pd
@@ -8,7 +32,8 @@ import time
 
 engine = create_engine("mysql+pymysql://root@localhost/dataAi")
 
-column = "Car_Name  Year    Selling_Price   Present_Price   Kms_Driven    Fuel_Type     Seller_Type     Transmission    Owner"
+import_column = "Car_Name  Year    Selling_Price   Present_Price   Kms_Driven    Fuel_Type     Seller_Type     Transmission    Owner    Fuel_TypeInt    Seller_Bool     Transmission_Bool"
+
 
 def chargement(x, link, table):
     
@@ -21,8 +46,11 @@ def chargement(x, link, table):
     df.to_sql(table, con = engine, if_exists='append', index=False)
     return print("Temps d execution : %s secondes ---" % (time.time() - start_time))
 
-
-chargement(column,'2020-11-02-brief-car/carData.csv', 'cardata')
-
+# Data cleaning on passe des bolean pour Transmission, SellerType et du Int pour Fuel.
 
 
+df['Fuel_TypeInt'] = df['Fuel_Type'].replace({'Petrol':0, 'CNG':1, 'Diesel':2})
+df['Seller_Bool'] = car_data['Seller_Type'].astype('category').cat.codes
+df['Transmission_Bool'] = df['Transmission'].replace({'Manual':1, 'Automatic':0})
+
+chargement(import_column,'carData.csv', 'cardata')
--- 2020-11-02-brief-car/script_import_sql.sql
+++ 2020-11-02-brief-car/script_import_sql.sql
@@ -3,14 +3,14 @@ use dataAi;
 drop table if exists cardata;
 
 create table cardata(
-Car_Name VARCHAR(100)
+Car_Name VARCHAR(50)
 ,Year INT(4)
 ,Selling_Price FLOAT(4)
 ,Present_Price FLOAT(4)
 ,Kms_Driven INT(10)
-,Fuel_Type VARCHAR(100) 
-,Seller_Type VARCHAR(100)
-,Transmission VARCHAR(100)
+,Fuel_Type VARCHAR(50) 
+,Seller_Type VARCHAR(50)
+,Transmission VARCHAR(50)
 ,Owner INT(5)
 )
 ; 
