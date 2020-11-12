d29732e6f23af0ad322dea2adcd632ef4e83ff64
ejhb <bonurajosh13@gmail.com>
2020-11-12 15:41:02 +0100

    update

--- 2020/11-november/12-bot-scrapp/scrappyyyy.py
+++ 2020/11-november/12-bot-scrapp/scrappyyyy.py
@@ -1,36 +1,11 @@
-# import scrapy
-
-# from scrapy.spiders import XMLFeedSpider
-# #from YahooScrape.items import YahooScrapeItem
-
-# class Spider(XMLFeedSpider):
-#     name = "YahooScrape"
-#     allowed_domains = ["yahoo.com"]
-#     start_urls = ('http://rss.jobsearch.monster.com/rssquery.ashx?q=big_data',)    #Crawl BPMX
-#     itertag = 'item'
-
-#     def parse_node(self, response, node):
-#         self.logger.info('Hi, this is a <%s> node!: %s', self.itertag, ''.join(node.extract()))
-
-#         item = {}
-#         item['title'] = node.xpath('title/text()',).extract_first()                #define XPath for title
-#         item['link'] = node.xpath('link/text()').extract_first()
-#         item['pubDate'] = node.xpath('link/pubDate/text()').extract_first()
-#         item['description'] = node.xpath('description/text()').extract_first()                #define XPath for description
-#         return item
-
-
 import scrapy
 
-linkeru = "./ascraper.xml"
-
 from scrapy.spiders import XMLFeedSpider
-#from YahooScrape.items import YahooScrapeItem
 
 class Spider(XMLFeedSpider):
-       name = "YahooScrape"
-       allowed_domains = ["yahoo.com"]
-       start_urls = (linkeru)    #Crawl BPMX
+       name = "BigDataScrap"
+       allowed_domains = ["http://rss.jobsearch.monster.com"]
+       start_urls = ['http://rss.jobsearch.monster.com/rssquery.ashx?q=big_data']    #Crawl BPMX
        itertag = 'item'
 
        def parse_node(self, response, node):
@@ -39,6 +14,6 @@ class Spider(XMLFeedSpider):
            item = {}
            item['title'] = node.xpath('title/text()',).extract_first()                #define XPath for title
            item['link'] = node.xpath('link/text()').extract_first()
-           item['pubDate'] = node.xpath('link/pubDate/text()').extract_first()
+        #    item['pubDate'] = node.xpath('link/pubDate/text()').extract_first()
            item['description'] = node.xpath('description/text()').extract_first()                #define XPath for description
            return item
