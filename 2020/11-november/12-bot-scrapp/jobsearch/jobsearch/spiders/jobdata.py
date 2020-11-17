bf897144bdea3e023032e07d5da2a566b6423d74
ejhb <bonurajosh13@gmail.com>
2020-11-17 11:11:47 +0100

    update

--- 2020/11-november/12-bot-scrapp/jobsearch/jobsearch/spiders/jobdata.py
+++ 2020/11-november/12-bot-scrapp/jobsearch/jobsearch/spiders/jobdata.py
@@ -2,9 +2,17 @@ import scrapy
 
 
 class JobdataSpider(scrapy.Spider):
-    name = 'jobdata'
-    allowed_domains = ['http://rss.jobsearch.monster.com']
-    start_urls = ['http://http://rss.jobsearch.monster.com/']
+        name = 'jobdata'
+        allowed_domains = ['rss.jobsearch.monster.com']
+        start_urls = ['http://rss.jobsearch.monster.com/rssquery.ashx?q=big_data']   #Crawl BPMX
+        itertag = 'item'
 
-    def parse(self, response):
-        pass
+       def parse_node(self, response, node):
+           self.logger.info('Hi, this is a <%s> node!: %s', self.itertag, ''.join(node.extract()))
+
+           item = {}
+           item['title'] = node.xpath('title/text()',).extract_first()      #define XPath for title
+           item['link'] = node.xpath('link/text()').extract_first()
+           item['pubDate'] = node.xpath('pubDate/text()').extract_first()
+           item['description'] = node.xpath('description/text()').extract_first()       #define XPath for description
+           return item
